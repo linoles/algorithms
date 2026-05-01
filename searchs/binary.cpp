ba9502d1c72b86bdf0a2c811879cea15d7b7ac25
linoles <cernikovdenis081@gmail.com>
2026-05-01 16:25:46 +0300

    Бинарный поиск

--- searchs/binary.cpp
+++ searchs/binary.cpp
@@ -13,7 +13,7 @@ using namespace std;
 int binary_search(vector<int> &nums, int query) {
   int l = 0; int r = size(nums) - 1; // Границы расссматриваемого массива
 
-  while (l < r) {
+  while (l <= r) {
     int mid = floor((l + r) / 2);
     int guess = nums[mid];
     if (guess == query) {
@@ -33,7 +33,7 @@ int main() {
   int n; // Кол-во чисел в массиве
   scanf("%d\n", &n);
 
-  vector<int> nums(n, 1); // Отсортированный массив
+  vector<int> nums; // Отсортированный массив
   for (int i = 0; i < n; i++) {
     int temp;
     scanf("%d ", &temp);
@@ -41,9 +41,9 @@ int main() {
   }
 
   int query;
-  scanf("%d\n", &query);
+  scanf("%d", &query);
 
-  printf("\nРезультат: %d", binary_search(nums, n));
+  printf("\nResult: %d", binary_search(nums, query));
 
   return 0;
 }
