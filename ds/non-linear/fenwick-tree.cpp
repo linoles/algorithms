228cb5d52a51af64563488c723f79bac512a1523
linoles <cernikovdenis081@gmail.com>
2026-05-10 13:48:35 +0300

    RUPQ + RURQ

--- ds/non-linear/fenwick-tree.cpp
+++ ds/non-linear/fenwick-tree.cpp
@@ -80,7 +80,7 @@ public:
   int select(int k)
   {
     int l = 1;
-    int r = ft.size();
+    int r = ft.size() - 1;
     int ans = ft.size() + 1;
 
     while (l <= r)
@@ -101,6 +101,103 @@ public:
   }
 };
 
+/*
+ * Range update and point query
+ * Обновление интервалов добавлением +val
+ * Получение конкретного query
+ */
+struct RUPQ
+{
+private:
+  vi ft;
+
+public:
+  RUPQ(int n)
+  {
+    ft.assign(n + 1, 0);
+  }
+
+  // Просто вывод diff
+  void cout_diff()
+  {
+    for (int x : ft)
+    {
+      cout << x << " ";
+    }
+  }
+
+  // Обновление одного числа O(log n)
+  void update(int i, int val)
+  {
+    while (i < ft.size())
+    {
+      ft[i] += val;
+      i += LSOne(i);
+    }
+  }
+
+  // Обновление интервала O(log n)
+  void range_update(int l, int r, int val)
+  {
+    update(l, val);
+    update(r + 1, -val);
+  }
+
+  // Получение query O(log n)
+  int point_query(int i)
+  {
+    int sum = 0;
+
+    while (i > 0)
+    {
+      sum += ft[i];
+      i -= LSOne(i);
+    }
+
+    return sum;
+  }
+};
+
+struct RURQ
+{
+private:
+  RUPQ bit1, bit2; // prefix(i) = i * query(B1, i) − query(B2, i)
+  int n;
+
+public:
+  RURQ(int n) : bit1(n), bit2(n), n(n) {};
+
+  // Обновление интервала O(log n)
+  void range_update(int l, int r, int val)
+  {
+    bit1.update(l, val);
+    bit1.update(r + 1, -val);
+
+    bit2.update(l, (l - 1) * val);
+    bit2.update(r + 1, -val * r);
+  }
+
+  // Префиксные суммы
+  int prefix_sum(int i)
+  {
+    return i * bit1.point_query(i) - bit2.point_query(i);
+  }
+
+  // Получение интервала O(log n)
+  int rsq(int l, int r)
+  {
+    return prefix_sum(r) - prefix_sum(l - 1);
+  }
+
+  // Простой вывод деревьев
+  void cout_rupq()
+  {
+    bit1.cout_diff();
+    cout << "\n";
+    bit2.cout_diff();
+  }
+};
+
 int main()
 {
   FenwickTree ft(8);
@@ -114,8 +211,51 @@ int main()
   cout << "sum [5...7] = " << ft.rsq(5, 7) << "\n";   // 9 + 11 + 13
   cout << "sum [3...3] = " << ft.rsq(3, 3) << "\n\n"; // 5
 
-  cout << "select(26) = " << ft.select(26) << "\n"; // Такое минимальное i, что [1...i] >= 26
-  cout << "select(49) = " << ft.select(49) << "\n"; // Такое минимальное i, что [1...i] >= 49
+  cout << "select(26) = " << ft.select(26) << "\n";   // Такое минимальное i, что [1...i] >= 26
+  cout << "select(49) = " << ft.select(49) << "\n\n"; // Такое минимальное i, что [1...i] >= 49
+
+  RUPQ rupq(8);
+
+  // 0 0 0 0 0 0 0 0 0 изначально
+  rupq.range_update(2, 5, 5); // 0 0 +5 0 0 0 -5 0 0
+                              //      ^        ^
+                              //      l       r+1
+                              //
+                              // l - левая граница, r - правая
+  rupq.cout_diff();
+  cout << "\n"; // Вывод для наглядности
+
+  rupq.range_update(3, 4, 3); // 0 0 +5 +3 +8 -3 -8 0 0
+                              //         ^     ^
+                              //         l    r+1
+                              //
+                              // l - левая граница, r - правая
+  rupq.cout_diff();
+  cout << "\n\n"; // Вывод для наглядности
+
+  cout << rupq.point_query(5) << "\n";
+  cout << rupq.point_query(3) << "\n\n\n";
+
+  RURQ rurq(8);
+
+  rurq.cout_rupq();
+  cout << "\n\n";
+
+  rurq.range_update(2, 5, 3); // Массив становится: 0 3 3 3 3 0 0 0
+  rurq.cout_rupq();           // Префиксные суммы: 0 3 6 9 12 12 12
+  cout << "\n\n";
+
+  rurq.range_update(4, 7, 5); // Массив становится: 0 3 3 8 8 5 5 0
+  rurq.cout_rupq();           // Префиксные суммы: 0 3 6 14 22 27 32 32
+  cout << "\n\n";
+
+  rurq.range_update(1, 4, 2); // Массив становится: 2 5 5 10 8 5 5 0
+  rurq.cout_rupq();           // Префиксные суммы: 2 7 12 22 30 35 40 40
+  cout << "\n\n\n";
+
+  cout << "rsq(3, 5): " << rurq.rsq(3, 5) << "\n"; // - - 5 10 8 - - -
+  cout << "rsq(4, 7): " << rurq.rsq(4, 7) << "\n"; // - - - 10 8 5 5 -
+  cout << "rsq(1, 8): " << rurq.rsq(1, 8);         // 2 5 5 10 8 5 5 0
 
   return 0;
 }
