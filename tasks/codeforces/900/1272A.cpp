2def7f158bc3861abfb6e63f27f86c71e6fcd07b
linoles <cernikovdenis081@gmail.com>
2026-05-18 19:24:32 +0300

    Codeforces *900 полнота 2 задачи + *800 1 задача

---
+++ tasks/codeforces/900/1272A.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// Codeforces 1272A (900) - Три друга, O(1)
+int main()
+{
+  int q, a, b, c; // 1 ≤ q ≤ 1000, 1 ≤ a, b, c ≤ 10^9
+  scanf("%d", &q);
+  while (q--)
+  {
+    scanf("%d %d %d", &a, &b, &c);
+
+    // Сортируем
+    if (b > c)
+      swap(b, c);
+    if (a > b)
+      swap(a, b);
+    if (b > c)
+      swap(b, c);
+
+    // Проверяем сначала, что нужно сделать с b
+    if (abs(c - b) > abs(b - a)) // Если b ближе к c, прибавляем
+      b++;
+    else if (abs(c - b) < abs(b - a)) // Если ближе к a, убавляем
+      b--;
+
+    // Дальше сокращаем дистанцию a и b
+    if (a < b)
+      a++;
+    // Теперь дистанцию между c и b или c и a
+    if (c > b || c > a)
+      c--;
+
+    // Важен именно такой порядок. Например, в случае 49 49 52 у меня возникал неправильный ответ
+
+    // Тут вроде расстояние не выйдет за INT_MAX, но я всё-таки подстраховался, т.к. 2 * 10^9 уже почти лимит
+    printf("%lld\n", (long long)(abs(a - b) + abs(a - c) + abs(b - c)));
+  }
+
+  return 0;
+}
---
+++ tasks/codeforces/900/1535B.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+// Codeforces 1535B (900) - Переупорядочение массива, O(n²)
+int main()
+{
+  int t, n;
+  scanf("%d", &t);
+  while (t--)
+  {
+    scanf("%d", &n);
+    int a[n + 1];
+    for (int i = 0; i < n; i++)
+      scanf("%d", &a[i]);
+
+    // Когда a[i] кратно 2, то gcd(a[i], 2 * a[j]) >= 2
+    // Когда a[i] не кратно 2, то gcd(a[i], 2 * a[j]) = gcd(a[i], a[j])
+    // Поэтому лучше расположить чётные числа в начале, а затем остальные
+    vector<int> sorted(a, a + n);
+    int last_even = -1;
+    for (int i = 0; i < n; i++)
+      if (sorted[i] % 2 == 0)
+      {
+        swap(sorted[i], sorted[last_even + 1]);
+        last_even++;
+      }
+
+    // Теперь просто считаем кол-во подходящих пар
+    int res = 0;
+    for (int i = 0; i < n - 1; i++)
+      for (int j = i + 1; j < n; j++)
+        if (gcd(sorted[i], 2 * sorted[j]) > 1)
+          res++;
+
+    printf("%d\n", res);
+  }
+
+  return 0;
+}
