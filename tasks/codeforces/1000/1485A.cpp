6f777cb4c834547ad336a6554bca883a43f6b5ea
linoles <cernikovdenis081@gmail.com>
2026-05-19 22:29:35 +0300

    Codeforces *1000 полнота 3 задачи

---
+++ tasks/codeforces/1000/1485A.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// Codeforces 1485A (1000) - Прибавляй и дели, O()
+int main()
+{
+  int t, a, b;
+  scanf("%d", &t);
+  while (t--)
+  {
+    scanf("%d %d", &a, &b);
+
+    // Если b = 1, то деление на 1 бесполезно
+    int opers = 0;
+    if (b == 1)
+    {
+      b++;
+      opers++;
+    }
+
+    // Находим наименьшее кол-во операций такое, что a / (b + k) = 0
+    int best_opers = INT_MAX;
+    for (int k = 0; k <= 30; k++)
+    {
+      int temp = a; // temp - int => деление будет округляться вниз
+      int cur_opers = 0;
+      while (temp >= 1)
+      {
+        temp /= b + k;
+        cur_opers++;
+      }
+
+      best_opers = min(best_opers, cur_opers + k);
+    }
+
+    printf("%d\n", opers + best_opers);
+  }
+
+  return 0;
+}
---
+++ tasks/codeforces/1000/1931C.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// Codeforces 1931C (1000) - Снова сделай равными, O(n)
+int main()
+{
+  int t, n;
+  scanf("%d", &t);
+  while (t--)
+  {
+    scanf("%d", &n);
+    int a[n + 1];
+    for (int i = 0; i < n; i++)
+      scanf("%d", &a[i]);
+
+    // Часть элементов массива уже должна быть x, тогда из что-то вроде x y y y x x мы
+    // должны получить x x x x x x
+
+    // Поэтому сначала ищем самую длинную последовательность в начале
+    int longest = 0;
+    for (int i = 0; a[i] == a[0] && i < n; i++)
+      longest++;
+
+    // Теперь, если в конце последовательность др. чисел, сравниваем длины, присваиваем наибольшую
+    if (a[n - 1] != a[0])
+    {
+      int end = 0;
+      for (int i = n - 1; a[i] == a[n - 1] && i > 0; i--)
+        end++;
+      longest = max(end, longest);
+    }
+    // Иначе и если весь массив не с одинаковыми числами, просто прибавляем в длине первой последовательности
+    // длину второй последовательности
+    else if (longest != n)
+      for (int i = n - 1; a[i] == a[n - 1] && i > 0; i--)
+        longest++;
+
+    printf("%d\n\n", n - longest);
+  }
+
+  return 0;
+}
